6e473965b4dbe109cc46f9aedf62e84b35770f6a
Pedro Gomes <pedrogomes94@gmail.com>
2020-11-27 20:56:17 +0100

    configure EIP155 jsonrpc

--- src/jsonrpc/eip155.ts
+++ src/jsonrpc/eip155.ts
@@ -3,7 +3,84 @@ import { JsonRpcConfig } from '@json-rpc-tools/utils';
 export const EIP155JsonRpc: JsonRpcConfig = {
   context: 'eip155',
   accounts: {
-    method: '',
+    method: 'eth_accounts',
   },
-  methods: {},
+  methods: {
+    eth_accounts: {
+      name: 'eth_accounts',
+      description: 'Get account addresses',
+      params: {
+        type: 'array',
+        items: {},
+      },
+      result: {
+        type: 'array',
+        items: {
+          type: 'string',
+        },
+      },
+    },
+    eth_chainId: {
+      name: 'eth_chainId',
+      description: 'Get EIP-155 chain id',
+      params: {
+        type: 'array',
+        items: {},
+      },
+      result: {
+        type: 'string',
+      },
+    },
+    eth_sendTransaction: {
+      name: 'eth_sendTransaction',
+      description: 'Creates, signs, and sends a new transaction to the network',
+      params: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            from: { type: 'string', required: true },
+            to: { type: 'string' },
+            gas: { type: 'string' },
+            gasPrice: { type: 'string' },
+            value: { type: 'string' },
+            data: { type: 'string' },
+            nonce: { type: 'string' },
+          },
+        },
+      },
+      result: {
+        type: 'string',
+      },
+      userApproval: true,
+    },
+    personal_sign: {
+      name: 'personal_sign',
+      description: 'Sign arbitrary message',
+      params: {
+        type: 'array',
+        items: {
+          type: 'string',
+        },
+      },
+      result: {
+        type: 'string',
+      },
+      userApproval: true,
+    },
+    eth_signTypedData: {
+      name: 'eth_signTypedData',
+      description: 'Sign type data message',
+      params: {
+        type: 'array',
+        items: {
+          type: 'string',
+        },
+      },
+      result: {
+        type: 'string',
+      },
+      userApproval: true,
+    },
+  },
 };
